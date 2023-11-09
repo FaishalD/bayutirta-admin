8340937a52b20612db8d3e9ebed9caedc1cd0111
FaishalD <faishald76@gmail.com>
2023-11-10 00:45:29 +0700

    feature:menambahkan sidebar

---
+++ resources/views/sidebar/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="side">
+    <div class="logo d-flex align-items-center gap-3">
+        <img src="/img/Vector.png" alt="Logo" />
+        Bayu Tirta 
+    </div>
+    <div class="divider"></div>
+    <div class="menu w-100 gap-2">
+        <a href="#" class="menu-col d-flex w-100 align-items-center gap-3 p-3" >
+            <ion-icon name="grid"></ion-icon>
+            Dashboard
+        </a>
+        <a href="/layanan" class="menu-col d-flex w-100 align-items-center gap-3 p-3">
+            <ion-icon name="bookmarks"></ion-icon>
+            Layanan
+        </a>
+        <a href="/reservasi" class="menu-col d-flex w-100 align-items-center gap-3 p-3">
+            <ion-icon name="calendar-number"></ion-icon>
+            Reservasi
+        </a>
+        <a href="artikel" class="menu-col d-flex w-100 align-items-center gap-3 p-3">
+            <ion-icon name="document-text"></ion-icon>
+            Artikel
+        </a>
+        <a href="ulasan" class="menu-col d-flex w-100 align-items-center gap-3 p-3">
+            <ion-icon name="ticket"></ion-icon>
+            Ulasan
+        </a>
+    </div>
+    <div class="acc d-flex position-relative">
+        <div class="role d-flex flex-column w-100 align-items-center position-absolute bottom-0 start-0">
+            <div class="position-absolute top-0 start-50 translate-middle">
+                <img src="/img/Vector.png" alt="logo">            
+            </div>
+            <h3> Admin Mas Eka </h3>
+            <p>Tukang Sambel</p>     
+        </div>
+    </div>
+</div>
