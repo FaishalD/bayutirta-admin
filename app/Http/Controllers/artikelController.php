3345a41db1b60595ab7d59ccd6e90d1278cc7f15
ridhoindra26 <indraridho26@gmail.com>
2023-12-28 14:52:38 +0700

    fix create artikel

--- app/Http/Controllers/artikelController.php
+++ app/Http/Controllers/artikelController.php
@@ -28,20 +28,25 @@ class artikelController extends Controller
     }
     public function addArtikel(Request $request)
     {
-        if($request->file('foto')){
-            $file= $request->file('foto');
-            $filename= date('YmdHi').$file->getClientOriginalName();
-            $file-> move(public_path('public/Image'), $filename);
-            $request['foto']= $filename;
-        }
-
         $validatedData = $request->validate([
             'judul' => 'required|max:255',
             'nama_penulis' => 'required',
             'title_penulis' => 'required',
-            'isi' => 'required',
-            'foto' => 'foto'
+            'isi' => 'required'
         ]);
+        
+        // if($request->file('foto')){
+            $file= $request->file('foto');
+            $filename= date('YmdHi').$file->getClientOriginalName()[0];
+            // Storage::disk('public')->url($filename);
+            // Storage::putFile('photos', new File('admin.bayutirta.masuk.id/public/Image'), $filename);
+            $file->storeAs('images', $filename, 'public');
+            // $file->move('admin.bayutirta.masuk.id/public/Image' , $filename);
+            // $file-> move(public_path(), $filename);
+            $request['foto']= $filename;
+            echo $filename;
+        // }
+
 
         // Create a new Post instance with the validated data
         $post = new Artikel([
@@ -49,7 +54,7 @@ class artikelController extends Controller
             'nama_penulis' => $validatedData['nama_penulis'],
             'title_penulis' => $validatedData['title_penulis'],
             'isi' => $validatedData['isi'],
-            'foto' => $validatedData['foto'],
+            'foto' => $filename,
             'created_at' => now()
         ]);
 
